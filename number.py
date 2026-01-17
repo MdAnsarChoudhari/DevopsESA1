1405c904ecdf85448b34a16aa54bab208d82cc6b
Muhammad Ansar M Choudhari <muhammadansarchoudhari@gmail.com>
2026-01-17 09:13:02 +0530

    save

--- number.py
+++ number.py
@@ -1,26 +1,21 @@
-import pytest
-
 def check_number(num):
-    if num%2==0:
-        even_odd="The Number is even "
+    if num % 2 == 0:
+        even_odd = "The Number is even"
     else:
-        even_odd="The Number is odd "
+        even_odd = "The Number is odd"
 
-    
-    count=0
-    for i in range(1, num+1):
-        if num%i==0:
-            count=count+1
+    count = 0
+    for i in range(1, num + 1):
+        if num % i == 0:
+            count += 1
 
-    if count==2:
-        prime="The number is prime"
+    if count == 2:
+        prime = "The Number is prime"
     else:
-        prime="The number is not prime"
+        prime = "The Number is not prime"
+
+    return [even_odd, prime]
 
 
-    return even_odd, prime
-
-
-if __name__=="__main__":
-
+if __name__ == "__main__":
     print(check_number(7))
--- tets_number.py
+++ tets_number.py
@@ -1,6 +1,6 @@
 from number import check_number
 
+def test_check_number_even_not_prime():
+    assert check_number(6) == ["The Number is even", "The Number is not prime"]
 
-def test_check_number():
 
-    assert check_number(6)==["The Number is even", "The Number is not prime"]
